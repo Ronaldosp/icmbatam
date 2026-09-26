2ec10031c7a6edbbdc76fecd1eb47cddfcefa6df
ronaldo_sp <ronaldosurya1507@gmail.com>
2026-09-26 11:34:56 +0700

    Initial backend

---
+++ doc.js
@@ -0,0 +1,5 @@
+// Admin User
+// npx sequelize-cli model:generate --name Admin --attributes username:string,email:string,password:string
+
+// Events Table
+// npx sequelize-cli model:generate --name Event --attributes title:string,date:date,thumbnail:string,venue:string,description:text
